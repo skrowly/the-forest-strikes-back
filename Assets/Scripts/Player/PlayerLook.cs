f1cf3ae94b3d3ceb3c4f4792a47a32ae5ed77253
Jackson Mitchell <jtmitc09@louisville.edu>
2026-04-25 18:24:21 -0400

    Create PlayerLook.cs

---
+++ Assets/Scripts/Player/PlayerLook.cs
@@ -0,0 +1,26 @@
+using UnityEngine;
+
+public class PlayerLook : MonoBehaviour
+{
+    public float sensitivity = 200f;
+    public Transform playerBody;
+
+    float xRotation = 0f;
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * sensitivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * sensitivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -80f, 80f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f, 0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
