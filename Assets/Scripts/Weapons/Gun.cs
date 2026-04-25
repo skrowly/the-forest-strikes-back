a68542ce2674537ed181bddf07c4853a24ef4738
Jackson Mitchell <jtmitc09@louisville.edu>
2026-04-25 18:59:44 -0400

    Create Gun.cs

---
+++ Assets/Scripts/Weapons/Gun.cs
@@ -0,0 +1,31 @@
+using UnityEngine;
+
+public class Gun : WeaponBase
+{
+    public Camera cam;
+
+    public override void Fire()
+    {
+        if (!CanFire()) return;
+
+        nextFireTime = Time.time + stats.fireRate;
+        ConsumeAmmo();
+
+        if (Physics.Raycast(cam.transform.position, cam.transform.forward, out RaycastHit hit, stats.range))
+        {
+            if (hit.collider.TryGetComponent(out EnemyHealth enemy))
+            {
+                enemy.TakeDamage(stats.damage);
+            }
+        }
+    }
+
+    public override void Reload()
+    {
+        int needed = stats.magSize - stats.currentAmmo;
+        int toReload = Mathf.Min(needed, stats.reserveAmmo);
+
+        stats.currentAmmo += toReload;
+        stats.reserveAmmo -= toReload;
+    }
+}
