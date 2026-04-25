91782596b8285526615ad00a32a3d36d053a5eac
Jackson Mitchell <jtmitc09@louisville.edu>
2026-04-25 19:15:01 -0400

    Create Projectile.cs

---
+++ Assets/Scripts/Weapons/Projectile.cs
@@ -0,0 +1,28 @@
+using UnityEngine;
+
+public class Projectile : MonoBehaviour
+{
+    public float speed = 30f;
+    public float damage = 20f;
+    public float lifetime = 3f;
+
+    void Start()
+    {
+        Destroy(gameObject, lifetime);
+    }
+
+    void Update()
+    {
+        transform.Translate(Vector3.forward * speed * Time.deltaTime);
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.TryGetComponent(out EnemyHealth enemy))
+        {
+            enemy.TakeDamage(damage);
+        }
+
+        Destroy(gameObject);
+    }
+}
